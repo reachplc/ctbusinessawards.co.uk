a0f021199326110122b56a83a3bc5d1543e5f54f
Michael Bragg <michael@michaelbragg.net>
2016-02-09 21:02:37 +0000

    Remove `the_post_navigation()` function until styled

--- html/app/themes/ctba-2016/single.php
+++ html/app/themes/ctba-2016/single.php
@@ -19,7 +19,7 @@ get_header(); ?>
 
 			get_template_part( 'content-parts/content', get_post_format() );
 
-			the_post_navigation();
+			#the_post_navigation();
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
