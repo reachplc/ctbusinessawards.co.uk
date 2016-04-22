e88b3018ed4338f91bcdf2302eb997411246bfbe
Michael Bragg <michael@michaelbragg.net>
2016-04-22 17:34:39 +0100

    Deploy: Update symlink paths

--- scripts/deploy-master.sh
+++ scripts/deploy-master.sh
@@ -20,10 +20,10 @@ ssh $USERNAME@$HOST "cd $DEPLOY_TO/releases/$TIMESTAMP/app/;ln -s $SHARED/media 
 ssh $USERNAME@$HOST "cd $DEPLOY_TO/releases/$TIMESTAMP/app/;ln -s $SHARED/languages $CURRENT/languages"
 
 ## Symlink previous events
-ssh $USERNAME@$HOST "ln -s $DEPLOY_TO/2012/ $CURRENT/2012"
-ssh $USERNAME@$HOST "ln -s $DEPLOY_TO/2013/ $CURRENT/2013"
-ssh $USERNAME@$HOST "ln -s $DEPLOY_TO/2014/ $CURRENT/2014"
-ssh $USERNAME@$HOST "ln -s $DEPLOY_TO/2015/ $DEPLOY_TO/releases/$TIMESTAMP/2015/"
+ssh $USERNAME@$HOST "ln -s $DEPLOY_TO/releases/$TIMESTAMP/2012/ $CURRENT/2012"
+ssh $USERNAME@$HOST "ln -s $DEPLOY_TO/releases/$TIMESTAMP/2013/ $CURRENT/2013"
+ssh $USERNAME@$HOST "ln -s $DEPLOY_TO/releases/$TIMESTAMP/2014/ $CURRENT/2014"
+ssh $USERNAME@$HOST "ln -s $DEPLOY_TO/releases/$TIMESTAMP/2015/ $CURRENT/2015/"
 
 # Update app version
 #ssh $USERNAME@$HOST "rm -rf $CURRENT;ln -s $DEPLOY_TO/releases/$TIMESTAMP/ $CURRENT"
