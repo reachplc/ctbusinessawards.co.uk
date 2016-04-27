d2c7223d95752e556ddf6eca9a3e5062f38ff1d4
Michael Bragg <michael@michaelbragg.net>
2016-04-27 14:56:21 +0100

    Fix Coding Standard issues

--- html/app/themes/ctba-2016/js/jquery.nomination.js
+++ html/app/themes/ctba-2016/js/jquery.nomination.js
@@ -1,9 +1,9 @@
-(function($) {
+(function( $ ) {
 
-	$( document ).ready( function() {
+	function toggleCategoryDisplay() {
 
 		// Get all check boxes that are not checked
-		var checkedValues = $('.cmb2-id-ctba-entries-2016-categories .cmb2-option:not(:checked)')
+		var checkedValues = $( '.cmb2-id-ctba-entries-2016-categories .cmb2-option:not( :checked )' )
 												 .map(function() {
 														return this.value;
 												 }).get();
@@ -13,28 +13,27 @@
 			$( '#' + value ).attr( 'data-state', 'hidden' );
 		});
 
-		// checkbox clicked function
-		$('.cmb2-id-ctba-entries-2016-categories .cmb2-checkbox-list input').click(function(){
+		// Checkbox clicked function
+		$( '.cmb2-id-ctba-entries-2016-categories .cmb2-checkbox-list input' ).click(function() {
+
+			// Get value from input box
+			var _catid = $( this ).val();
 
 			// Add visible class to selected checkbox
-			$(this).parent('li').toggleClass('is_visible');
+			$( this ).parent( 'li' ).toggleClass( 'is_visible' );
 
-		  // get value from input box
-		  var _catid = $(this).val();
-			console.log('selected', _catid );
-
-			// show question id from value checked
-			if ( $(this).is(':checked') ) {
+			// Show question id from value checked
+			if ( $( this ).is( ':checked' ) ) {
 				$( '#' + _catid ).attr( 'data-state', 'visible' );
 			}
 
-			// hide question id from value unchecked
-			if( ! $(this).is(':checked') ){
+			// Hide question id from value unchecked
+			if ( ! $( this ).is( ':checked' ) ) {
 				$( '#' + _catid ).attr( 'data-state', 'hidden' );
 			}
-
 		});
+	}
 
-	});
+	$( document ).ready( toggleCategoryDisplay() ); // $( document ).ready
 
 } )( jQuery );
