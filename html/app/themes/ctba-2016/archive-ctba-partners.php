80c1adb439cf32ca1c6138550c83f330e9db08ed
Michael Bragg <michael@michaelbragg.net>
2016-04-28 14:09:40 +0100

    Update partners page

--- html/app/themes/ctba-2016/archive-ctba-partners.php
+++ html/app/themes/ctba-2016/archive-ctba-partners.php
@@ -15,12 +15,14 @@ get_header(); ?>
 		  <div class="wrapper__sub">
 		    <article class="content__main ss1-ss4 ms1-ms6 ls1-ls8 separator">
 
-		<?php
-		if ( have_posts() ) : ?>
-			<header class="page-header">
+	<?php
+	if ( have_posts() ) :
+		// @TODO Move copy to plugin
+	?>
+			<header class="page-header box">
 				<h1 class="gamma heading--main page-title">2016 Partners</h1>
 				<h2>Become a partner:</h2>
-				<p>For more information regarding sponsorship opportunities, call Craig Cooksley on <a href="tel:01212345785">0121 234 5785</a> / <a href="tel:07826893264">07826 893 264</a> or email <a href="mailto:craig.cooksley@trinitymirror.com">craig.cooksley@trinitymirror.com</a>.</p>
+				<p>For more information regarding sponsorship opportunities, call Michelle Gardiner on <a href="tel:08453313031">08453 31 30 31</a> / <a href="tel:07791122627">07791 122 627</a> or email <a href="mailto:mgardiner@championsukplc.com">mgardiner@championsukplc.com</a>.</p>
 			</header><!-- .page-header -->
 
   <ul class="judges judges__columns list">
@@ -33,34 +35,35 @@ get_header(); ?>
 			  <aside class="block__aside partners-item__aside">
 			    <?php if ( has_post_thumbnail() ) : ?>
 			    <a href="<?php echo esc_url( get_permalink() ); ?>">
-			      <?php the_post_thumbnail(
-			    	'logo-parter',
-			    	array(
-			    		'class' => 'image__responsive'
-			    	)
-			    ); ?>
-			    </a>
-			  <?php endif; ?>
+					<?php the_post_thumbnail(
+						'logo-parter',
+						array(
+							'class' => 'image__responsive',
+						)
+					); ?>
+					</a>
+					<?php endif; ?>
 			  </aside>
 			  <articles class="block__content partners-item__content">
 			    <h3><?php the_title(); ?></h3>
-			    <!--<p>{{ partner.description }}</p>-->
-			    <p><a href="<?php echo esc_url( get_permalink() ); ?>">View Profile</a><?php if( get_field('ctba-partners-url')):?> | <a target="_blank" class="outbound link" href="<?php echo get_field('ctba-partners-url'); ?>" rel="nofollow">View Website</a><?php endif; ?></p>
+			    <p>
+						<a href="<?php echo esc_url( get_permalink() ); ?>">View Profile</a><?php if ( get_field( 'ctba-partners-url' ) ) : ?> | <a target="_blank" class="outbound link" href="<?php echo esc_url( get_field( 'ctba-partners-url' ) ); ?>" rel="nofollow">View Website</a><?php endif; ?>
+					</p>
 			  </articles>
 			</li>
 
 			<?php endwhile; ?>
 			</ul>
- <?php else :
+	<?php else : ?>
 
-			get_template_part( 'content-parts/content', 'none' );
+		<?php get_template_part( 'content-parts/content', 'none' ); ?>
 
-		endif; ?>
+	<?php endif; ?>
 
 		    </article>
 
 		    <aside class="content__aside ss1-ss4 ms1-ms6 ls9-ls12">
-		      <?php get_sidebar(); ?>
+				<?php get_sidebar(); ?>
 		    <aside>
 
 		  </div>
