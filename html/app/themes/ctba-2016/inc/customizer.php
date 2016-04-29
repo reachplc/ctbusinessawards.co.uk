6d115e21e3dbc50ef99e9fd431e48814146ad2e2
Michael Bragg <michael@michaelbragg.net>
2016-04-29 13:42:32 +0100

    Fix for custom logo

--- html/app/themes/ctba-2016/inc/customizer.php
+++ html/app/themes/ctba-2016/inc/customizer.php
@@ -13,7 +13,6 @@
 function ctba_2016_customize_register( $wp_customize ) {
 	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
-	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
 }
 add_action( 'customize_register', 'ctba_2016_customize_register' );
 
