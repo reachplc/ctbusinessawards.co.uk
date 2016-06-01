a6ff1e9a891bebb5a4f70a6ff0a5a0f4034e6e2b
Michael Bragg <michael@michaelbragg.net>
2016-06-01 13:59:03 +0100

    Improve registration email content

--- html/app/plugins-mu/ti-core-functions.php
+++ html/app/plugins-mu/ti-core-functions.php
@@ -27,6 +27,16 @@ class TI_Core_Functions {
 			array( $this, 'ti_add_home_to_menu' )
 		);
 
+		remove_action(
+			'wpmu_new_user',
+			'newuser_notify_siteadmin'
+		);
+
+		add_action(
+			'wpmu_new_user',
+			array( $this, 'newuser_notify_siteadmin_enhanced' )
+		);
+
 	}
 
 	/**
@@ -57,6 +67,65 @@ class TI_Core_Functions {
 			'dashicons-admin-home',
 			4
 		);
+
+	}
+
+	/**
+	 * Add extra information to registration email
+	 *
+	 * (https://buddydev.com/buddypress/enhancing-the-new-user-registration-message-on-wordpress-multisite-and-buddypress-to-make-it-more-informative-for-site-admins/)
+	 * @since 0.1.0
+	 */
+
+	public function newuser_notify_siteadmin_enhanced( $user_id ) {
+
+		if ( 'yes' !== get_site_option( 'registrationnotification' ) ) {
+			return false;
+		}
+
+		$email = get_site_option( 'admin_email' );
+
+		if ( false === is_email( $email ) ) {
+			return false;
+		}
+
+		$user = new WP_User( $user_id );
+
+		if ( function_exists( 'bp_core_get_user_domain' ) ) { // Just make sure to not cause trouble when bp is disables.
+			$user_link = bp_core_get_user_domain( $user_id );
+		} else {
+			$user_link = network_admin_url( 'user-edit.php?user_id=' . $user_id );//just making sure it works on normal wpms installs too
+		}
+
+		$options_site_url = esc_url( network_admin_url( 'ms-options.php' ) );
+
+		$msg = sprintf(
+			__('New User: %1s
+			User email: %2s
+			View Profile: %3s
+
+			Disable these notifications: %4s'),
+			$user->user_login,
+			$user->user_email,
+			$user_link,
+			$options_site_url
+		);
+
+		$msg = apply_filters( 'newuser_notify_siteadmin_enhanced', $msg, $user );
+
+		wp_mail(
+			$email,
+			sprintf(
+				apply_filters(
+					'new_user_registration_message_subject',
+					__( 'New User Registration: %s' )
+				),
+				$user->user_login
+			),
+			$msg
+		);
+
+		return true;
 
 	}
 
